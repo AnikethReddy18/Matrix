978069b053f5e6cb79e2002893c840d634436b8b
sunny <sunnyrockzzz1811@gmail.com>
2023-08-10 13:32:44 +0530

    Added a way to make matrix and few other changes

--- Matrix.py
+++ Matrix.py
@@ -9,6 +9,30 @@ class Matrix:
         for row in matrix_rows:
             print(row)
 
+    def element(self, column=1, row=1):
+        r1 = self.r1
+        r2 = self.r2
+        r3 = self.r3
+
+        element = None
+        if row == 1:
+            element = r1[column - 1]
+        elif row == 2:
+            element = r2[column - 1]
+        elif row == 3:
+            element = r3[column - 1]
+
+        return element
+
+    def make_matrix(self, numbers):
+        """PUT IN A STRING OF NUMBER SEPARATED BY A COMMA AND NO SPACES """
+        matrix_fr = numbers.split(',')
+        r1 = matrix_fr[:3]
+        r2 = matrix_fr[3:6]
+        r3 = matrix_fr[6:9]
+        new_matrix = Matrix(r1, r2, r3)
+        new_matrix.print_matrix()
+
 
 class OperateMatrix:
     def __init__(self, matrix1, matrix2):
@@ -22,5 +46,17 @@ class OperateMatrix:
         r1 = [m1.r1[0] + m2.r1[0], m1.r1[1] + m2.r1[1], m1.r1[2] + m2.r1[2]]
         r2 = [m1.r2[0] + m2.r2[0], m1.r2[1] + m2.r2[1], m1.r2[2] + m2.r2[2]]
         r3 = [m1.r3[0] + m2.r3[0], m1.r3[1] + m2.r3[1], m1.r3[2] + m2.r3[2]]
+
+        new_matrix = Matrix(r1, r2, r3)
+        return new_matrix
+
+    def sub_matrix(self):
+        m1 = self.m1
+        m2 = self.m2
+
+        r1 = [m1.r1[0] - m2.r1[0], m1.r1[1] - m2.r1[1], m1.r1[2] - m2.r1[2]]
+        r2 = [m1.r2[0] - m2.r2[0], m1.r2[1] - m2.r2[1], m1.r2[2] - m2.r2[2]]
+        r3 = [m1.r3[0] - m2.r3[0], m1.r3[1] - m2.r3[1], m1.r3[2] - m2.r3[2]]
+
         new_matrix = Matrix(r1, r2, r3)
         return new_matrix
