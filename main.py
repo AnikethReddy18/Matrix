65d427761456304ccca524f71d8bf493b9097260
sunny <sunnyrockzzz1811@gmail.com>
2023-08-11 23:56:58 +0530

    Changed the layout system to grid for more control over the layout

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from Matrix import Matrix, OperateMatrix
 import tkinter as tk
 
+window =tk.Tk()
+window.title("Matrix Calculator")
+window.geometry("600x400")
+
 matrix1 = Matrix()
 matrix2 = Matrix()
 operation = OperateMatrix()
@@ -16,11 +20,9 @@ def construct_matrices():
     matrix1 = matri1
     matrix2 = matri2
 
-    operation = OperateMatrix(matrix1, matrix2)
-
-
-def show_matrices():
     show_matrix.config(text=f"{matrix1}and\n{matrix2} are your input matrices")
+
+    operation = OperateMatrix(matrix1, matrix2)
 
 
 def add():
@@ -35,7 +37,6 @@ def mul():
     result.config(text=operation.mul_matrix())
 
 
-window = tk.Tk()
 
 instructions = tk.Label(text="To make a matrix: Enter 9 numbers of your choice separated by a comma\n"
                              "and no space. They will be filled left to right starting from top to bottom")
@@ -44,21 +45,19 @@ input1 = tk.Entry()
 input2 = tk.Entry()
 construct_matrix = tk.Button(text="CONSTRUCT MATRICES", command=construct_matrices)
 show_matrix = tk.Label()
-show_button = tk.Button(text="Show Matrices", command=show_matrices)
 add_button = tk.Button(text="add", command=add)
 sub_button = tk.Button(text="subtract", command=sub)
 mul_button = tk.Button(text="multiply", command=mul)
 result = tk.Label()
 
-instructions.pack()
-input1.pack()
-input2.pack()
-construct_matrix.pack()
-show_button.pack()
-show_matrix.pack()
-add_button.pack()
-sub_button.pack()
-mul_button.pack()
-result.pack()
+instructions.grid(column=0,row=0)
+input1.grid(column=0, row=1, sticky="w")
+input2.grid(column=0, row=2, sticky="w")
+construct_matrix.grid(column=0, row=3, sticky="w")
+show_matrix.grid(column=1,row=1)
+add_button.grid(column=0, row=4, sticky="w")
+sub_button.grid(column=0, row=5, sticky="w")
+mul_button.grid(column=0, row=6, sticky="w")
+result.grid(column=1, row=4)
 
 window.mainloop()
